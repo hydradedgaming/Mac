634a44c3883d0d443cb8e426363bd2a97aa7c6ae
hydradedgaming <167041346+hydradedgaming@users.noreply.github.com>
2024-05-17 18:50:58 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,18 @@
+
+Copy code
+#!/bin/bash
+
 say_hi() {
     echo -e "\033[32mhi\033[0m"
 }
 
 main() {
-    read -p "Enter text: " text
-    if [ "$text" == "yes" ] || [ "$text" == "Yes" ]; then
+
+    echo -n "Enter text: "
+    read -r text
+    
+    # Check if input is "yes" or "Yes"
+    if [[ "$text" == "yes" || "$text" == "Yes" ]]; then
         echo -e "\033[32mok hi bro\033[0m"
     else
         say_hi
