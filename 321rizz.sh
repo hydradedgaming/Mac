d6a2d8447d13c77a12cc8bddd458e802d4963abd
hydradedgaming <167041346+hydradedgaming@users.noreply.github.com>
2024-05-18 07:04:16 -0400

    Update and rename rizz123.sh to 321rizz.sh

--- 321rizz.sh
+++ 321rizz.sh
@@ -21,7 +21,7 @@ echo ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
 
  echo  "[Abyss] Digital Software"
  echo "[Owner] Nexus42
-
+ echo "Made By Hydradedgaming DM Me You Theme Ideas On Discord Discord User : Hydradedgaming And Join The Discord https://discord.gg/NASGRZa5NT"
     echo "Type Yes If You Would Like To Install The Theme"
     read text
  
