6e62739dbf592ebb2d23b295a6a155823882f020
hydradedgaming <167041346+hydradedgaming@users.noreply.github.com>
2024-05-18 07:03:06 -0400

    Update and rename idk.sh to rizz123.sh

--- rizz123.sh
+++ rizz123.sh
@@ -1,3 +1,4 @@
+main() {
 echo ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣾⣿⣿⣷⣶⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 echo ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀
 echo ⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⠀⠀⠀⠀⠀
@@ -20,15 +21,16 @@ echo ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
 
  echo  "[Abyss] Digital Software"
  echo "[Owner] Nexus42
-main() {
-    echo " Type Yes If You Would Like To Install The Theme "
+
+    echo "Type Yes If You Would Like To Install The Theme"
     read text
  
     if [[ "$text" == "yes" || "$text" == "Yes" ]]; then
-   echo "farts"
+  curl -s "https://raw.githubusercontent.com/hydradedgaming/MacSploit_Themes/main/CatFall.sh" | bash
     else
         echo -e"Command Not Registered Try Again"  
-        echo "Made By Hydradedgaming DM Me You Theme Ideas On Discord Discord User = Hydradedgaming And Join The Disocrd :D https://discord.gg/NASGRZa5NT"
+        
+        echo "Made By Hydradedgaming DM Me You Theme Ideas On Discord Discord User : Hydradedgaming And Join The Discord https://discord.gg/NASGRZa5NT"
 }
 
 main
