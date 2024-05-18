b7bd8d1ed98738091d5b9bf9407178c4c99ab94f
hydradedgaming <167041346+hydradedgaming@users.noreply.github.com>
2024-05-18 11:04:55 -0400

    Update and rename 123.sh to 321.sh

--- 321.sh
+++ 321.sh
@@ -1,8 +1,8 @@
 main() {
     echo -e "Made By Hydradedgaming"
     read text
-    echo "Type ok or fine to install the theme"
-    if [[ "$text" == "ok" || "$text" == "fine" ]]; then
+    echo "Type ok or fine to install the theme"                     
+    if [[ "$text" == "yes" || "$text" == "Yes" ]]; then
        cd /Applications/MacSploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle
         curl "https://raw.githubusercontent.com/hydradedgaming/MacSploit_Themes/main/GetDaTheme.js" -o "./theme-tomorrow_night.js"
     fi
@@ -25,10 +25,10 @@ main() {
     echo "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿"
     echo "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿"
     echo "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿"
-    echo "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿"
 
     echo "[Abyss] Digital Software"
     echo "[Owner] Nexus42"
+    sleep(1)
     echo "Install Completed DM Me On Discord With Theme Ideas And Join The Discord : https://discord.gg/NASGRZa5NT"
 }
 main
