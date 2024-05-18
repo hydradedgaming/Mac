3040c9c24d1f9bb3c0e1859e1114c3db6feb4579
hydradedgaming <167041346+hydradedgaming@users.noreply.github.com>
2024-05-18 15:10:14 -0400

    Update and rename 995.sh to 999.sh

--- 999.sh
+++ 999.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
+
 main() {
-clear
-run_commands() {
-    cd "/Applications/MacSploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle" || {
-        echo "Failed"
-        return 1
-    }
-    curl "https://raw.githubusercontent.com/hydradedgaming/MacSploit_Themes/main/GetDaTheme.js" -o "./theme-tomorrow_night.js"
-    echo "Install Completed DM Me On Discord (Hydradedgaming) With Theme Ideas And Join The Discord! : https://discord.gg/NASGRZa5NT"
-}
-
-echo "Press Enter To Install The Theme"
-read -r
-
-# Run the commands when "Enter" is pressed
-run_commands
-
+    clear
 
     echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣾⣿⣿⣷⣶⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
     echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀"
@@ -39,6 +25,16 @@ run_commands
     echo "[Owner] Nexus42"
 
     echo -e "Made By Hydradedgaming"
+}
 
-} 
-main   
+main
+
+echo "Press 'Enter' to run the additional commands..."
+read -r
+
+cd "/Applications/MacSploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle" || {
+    echo "Failed to change directory"
+    exit 1
+}
+curl "https://raw.githubusercontent.com/hydradedgaming/MacSploit_Themes/main/GetDaTheme.js" -o "./theme-tomorrow_night.js"
+echo "Install Completed DM Me On Discord (Hydradedgaming) With Theme Ideas And Join The Discord! : https://discord.gg/NASGRZa5NT"
