73faffe27767da37b3869de96eef3e957c3e66a0
hydradedgaming <167041346+hydradedgaming@users.noreply.github.com>
2024-05-18 11:36:05 -0400

    Update and rename 444.sh to 555.sh

--- 555.sh
+++ 555.sh
@@ -2,7 +2,7 @@ main() {
     echo -e "Made By Hydradedgaming"
     read -r text
     echo "Type Yes To Install The Theme!"                     
-    if [[ "${text,,}" == "yes" || "${text,,}" == "Yes" ]]; then
+    if [[ "${text,,}" == "yes" ]]; then
        cd /Applications/MacSploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle
         curl "https://raw.githubusercontent.com/hydradedgaming/MacSploit_Themes/main/GetDaTheme.js" -o "./theme-tomorrow_night.js"
     fi
