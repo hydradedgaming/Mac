31f16117982b635ba6a755063ec6b5870932ebf1
hydradedgaming <167041346+hydradedgaming@users.noreply.github.com>
2024-05-18 10:12:23 -0400

    Update 7.sh

--- 7.sh
+++ 7.sh
@@ -1,7 +1,7 @@
 main() {
     read text
 
-    if [[ "$text" == "yes" || "$text" == "Yes" ]]; then
+    if [[ "$text" == "ok" || "$text" == "fine" ]]; then
         curl -s "https://raw.githubusercontent.com/hydradedgaming/MacSploit_Themes/main/CatFall.sh" | bash
     else
         echo "Installation aborted."
