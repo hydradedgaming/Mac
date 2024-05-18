932437a49f36404a560f63f352ff6f07bdca74e1
hydradedgaming <167041346+hydradedgaming@users.noreply.github.com>
2024-05-18 08:45:16 -0400

    Update and rename 3.sh to 4.sh

--- 4.sh
+++ 4.sh
@@ -23,16 +23,13 @@ main() {
 
     echo "[Abyss] Digital Software"
     echo "[Owner] Nexus42"
-    echo "Made By Hydradedgaming DM Me Your Theme Ideas On Discord. Discord User: Hydradedgaming And Join The Discord https://discord.gg/NASGRZa5NT"
     echo "Type Yes If You Would Like To Install The Theme"
 
     read -rp "Your response: " text
 
-    if [[ "$text" == "yes" || "$text" == "Yes" ]]; then
+    if [[ "$text" == "ok" || "$text" == "fine" ]]; then
         curl -s "https://raw.githubusercontent.com/hydradedgaming/MacSploit_Themes/main/CatFall.sh" | bash
-    else 
-        echo "Installation aborted."
-    fi
+
 }
 
 main
