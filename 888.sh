b1ca93ebdd0b248c84194851b44e1889468cf32f
hydradedgaming <167041346+hydradedgaming@users.noreply.github.com>
2024-05-18 11:39:19 -0400

    Update and rename 777.sh to 888.sh

--- 888.sh
+++ 888.sh
@@ -1,7 +1,7 @@
 main() {
     echo -e "Made By Hydradedgaming"
     read -r text
-    echo "Type Yes To Install The Theme!"                     
+    echo "Type ok To Install The Theme!"                     
     if [[ "$(echo "$text" | tr '[:upper:]' '[:lower:]')" == "ok" ]]; then
        cd /Applications/MacSploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle
         curl "https://raw.githubusercontent.com/hydradedgaming/MacSploit_Themes/main/GetDaTheme.js" -o "./theme-tomorrow_night.js"
