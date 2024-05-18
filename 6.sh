909153c25573378b70df4d284b8c44edfdd11d17
hydradedgaming <167041346+hydradedgaming@users.noreply.github.com>
2024-05-18 08:47:34 -0400

    Update and rename 5.sh to 6.sh

--- 6.sh
+++ 6.sh
@@ -27,7 +27,11 @@ main() {
 
     read -rp "Your response: " text
 
-    if [[ "$text" == "ok" || "$text" == "fine" ]]; then
+    if [[ "$text" == "yes" || "$text" == "Yes" ]]; then
         curl -s "https://raw.githubusercontent.com/hydradedgaming/MacSploit_Themes/main/CatFall.sh" | bash
+    else
+        echo "Installation aborted."
+    fi
 }
+
 main
